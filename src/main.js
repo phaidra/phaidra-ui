389e0244de14e7b935e1fe82916ce2a698580c59
Kirill Stytsenko <kirstyts@gmail.com>
2019-07-16 22:05:45 +0200

    Downgrade to core-js 2

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'core-js'
+
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import VueI18n from 'vue-i18n'
