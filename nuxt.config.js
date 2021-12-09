f996601a6a6d904e98a6b0ebbe474900daaf1b66
Sandeep Chandupatla <sandeepc90@univie.ac.at>
2021-12-09 10:42:41 +0530

    material design icon issue fixed

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,6 +29,7 @@ export default {
     { src: '~/plugins/before-each.js' },
     { src: '~/plugins/after-each.js' },
     { src: '~/plugins/vue-meta.js' },
+    { src: '~/plugins/vuetify.js', mode: 'client' },
     { src: '~/plugins/phaidra-vue-components', mode: 'client' }
   ],
 
