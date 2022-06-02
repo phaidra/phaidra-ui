c357487597649850ad8e5502a7949788869e1c68
Phaidra Admin <adminp7@app02.cc.univie.ac.at>
2022-06-02 13:59:22 +0200

    set cookie to top domian in case the ui is running on ui. subdomain

--- store/index.js
+++ store/index.js
@@ -569,7 +569,7 @@ export const actions = {
         commit('setToken', null)
         commit('setLoginData', { username: null, firstname: null, lastname: null, email: null, org_units_l1: null, org_units_l2: null })
         if (process.browser) {
-          document.cookie = 'X-XSRF-TOKEN=; domain=' + window.location.hostname + '; path=/; secure; samesite=strict; expires=Thu, 01 Jan 1970 00:00:01 GMT'
+          document.cookie = 'X-XSRF-TOKEN=; domain=' + window.location.hostname.replace('ui.', '') + '; path=/; secure; samesite=strict; expires=Thu, 01 Jan 1970 00:00:01 GMT'
         }
       }
     }
@@ -577,7 +577,7 @@ export const actions = {
   async login({ commit, dispatch, state }, credentials) {
     commit('clearStore')
     if (process.browser) {
-      document.cookie = 'X-XSRF-TOKEN=; domain=' + window.location.hostname + '; path=/; secure; samesite=strict; expires=Thu, 01 Jan 1970 00:00:01 GMT'
+      document.cookie = 'X-XSRF-TOKEN=; domain=' + window.location.hostname.replace('ui.', '') + '; path=/ ; secure; samesite=strict; expires=Thu, 01 Jan 1970 00:00:01 GMT'
     }
     commit('setUsername', credentials.username)
     try {
@@ -591,7 +591,7 @@ export const actions = {
       }
       if (response.status === 200) {
         if (process.browser) {
-          document.cookie = 'X-XSRF-TOKEN=' + response.data['XSRF-TOKEN'] + '; domain=' + window.location.hostname + '; path=/; secure; samesite=strict'
+          document.cookie = 'X-XSRF-TOKEN=' + response.data['XSRF-TOKEN'] + '; domain=' + window.location.hostname.replace('ui.', '') + '; path=/; secure; samesite=strict'
         }
         commit('setToken', response.data['XSRF-TOKEN'])
         dispatch('getLoginData')
@@ -603,7 +603,7 @@ export const actions = {
     commit('clearAlerts')
     this.$cookies.remove('user')
     if (process.browser) {
-      document.cookie = 'X-XSRF-TOKEN=; domain=' + window.location.hostname + '; path=/; secure; samesite=strict; expires=Thu, 01 Jan 1970 00:00:01 GMT'
+      document.cookie = 'X-XSRF-TOKEN=; domain=' + window.location.hostname.replace('ui.', '') + '; path=/; secure; samesite=strict; expires=Thu, 01 Jan 1970 00:00:01 GMT'
     }
     try {
       const response = await axios.get(state.instanceconfig.api + '/signout', {
