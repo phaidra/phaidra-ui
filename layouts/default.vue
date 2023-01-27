7bb84c7d932193b90cd001c39159abaf9d957fde
Sandeep Chandupatla <sandeepc90@univie.ac.at>
2023-01-27 11:39:08 +0530

    other meta tags with dynamic meta tag

--- layouts/default.vue
+++ layouts/default.vue
@@ -78,6 +78,11 @@ export default {
   metaInfo() {
     let metaInfo = {
       title: config.title,
+      meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+      ]
     };
     return metaInfo;
   },
