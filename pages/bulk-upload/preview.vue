ab45ea98a99662b697e6ac2d02ee2b0f4db89f10
davegson <3080765+davegson@users.noreply.github.com>
2025-03-06 16:12:50 +0100

    Handle multi-fields in preview

--- pages/bulk-upload/preview.vue
+++ pages/bulk-upload/preview.vue
@@ -7,76 +7,124 @@
       </v-col>
     </v-row>
 
-    <v-row>
-      <v-col>
-        <v-card outlined>
-          <v-card-title>Data Preview</v-card-title>
-          <v-card-text>
-            <v-simple-table>
-              <template v-slot:default>
-                <thead>
-                  <tr>
-                    <th v-for="field in allFields" :key="field" class="text-left">
-                      <div class="d-flex align-center">
-                        {{ field }}{{ fieldSettings[field].required ? '*' : '' }}
-                        <v-tooltip bottom dark>
-                          <template v-slot:activator="{ on, attrs }">
-                            <v-icon 
-                              x-small 
-                              class="ml-1"
-                              :class="{ 'grey--text': !getAllFieldMappings[field] }"
-                              v-bind="attrs"
-                              v-on="on"
-                            >
-                              mdi-information
-                            </v-icon>
-                          </template>
-                          {{ getSourceInfo(field) || 'Not sourced (optional)' }}
-                        </v-tooltip>
-                      </div>
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr v-for="(row, index) in previewData" :key="index">
-                    <td 
-                      v-for="field in allFields" 
-                      :key="field"
-                      :class="{ 'grey--text': !getAllFieldMappings[field] }"
-                    >
-                      {{ row[field] || '-' }}
-                    </td>
-                  </tr>
-                </tbody>
-              </template>
-            </v-simple-table>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
+    <template v-if="isInitialized">
+      <v-row>
+        <v-col>
+          <v-card outlined>
+            <v-card-title>Data Preview</v-card-title>
+            <v-card-text>
+              <v-simple-table>
+                <template v-slot:default>
+                  <thead>
+                    <tr>
+                      <template v-for="field in allFields">
+                        <!-- Handle multi-fields -->
+                        <template v-if="fieldSettings[field].fieldType === 'multi-field'">
+                          <th 
+                            v-for="(subFieldConfig, subField) in fieldSettings[field].multiFieldConfig.fields"
+                            :key="field + '-' + subField"
+                            class="text-left"
+                          >
+                            <div class="d-flex align-center">
+                              {{ field }}: {{ subField }}{{ subFieldConfig.required ? '*' : '' }}
+                              <v-tooltip bottom dark>
+                                <template v-slot:activator="{ on, attrs }">
+                                  <v-icon 
+                                    x-small 
+                                    class="ml-1"
+                                    :class="{ 'grey--text': !getSubFieldMapping(field, subField) }"
+                                    v-bind="attrs"
+                                    v-on="on"
+                                  >
+                                    mdi-information
+                                  </v-icon>
+                                </template>
+                                {{ getSourceInfo(field, subField) || 'Not sourced (optional)' }}
+                              </v-tooltip>
+                            </div>
+                          </th>
+                        </template>
+                        <!-- Handle single fields -->
+                        <th 
+                          v-else
+                          :key="field"
+                          class="text-left"
+                        >
+                          <div class="d-flex align-center">
+                            {{ field }}{{ fieldSettings[field].required ? '*' : '' }}
+                            <v-tooltip bottom dark>
+                              <template v-slot:activator="{ on, attrs }">
+                                <v-icon 
+                                  x-small 
+                                  class="ml-1"
+                                  :class="{ 'grey--text': !getAllFieldMappings[field] }"
+                                  v-bind="attrs"
+                                  v-on="on"
+                                >
+                                  mdi-information
+                                </v-icon>
+                              </template>
+                              {{ getSourceInfo(field) || 'Not sourced (optional)' }}
+                            </v-tooltip>
+                          </div>
+                        </th>
+                      </template>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr v-for="(row, index) in previewData" :key="index">
+                      <template v-for="field in allFields">
+                        <!-- Handle multi-fields -->
+                        <template v-if="fieldSettings[field].fieldType === 'multi-field'">
+                          <td 
+                            v-for="(subFieldConfig, subField) in fieldSettings[field].multiFieldConfig.fields"
+                            :key="field + '-' + subField"
+                            :class="{ 'grey--text': !getSubFieldMapping(field, subField) }"
+                          >
+                            {{ row[field]?.[subField] || '-' }}
+                          </td>
+                        </template>
+                        <!-- Handle single fields -->
+                        <td 
+                          v-else
+                          :key="field"
+                          :class="{ 'grey--text': !getAllFieldMappings[field] }"
+                        >
+                          {{ row[field] || '-' }}
+                        </td>
+                      </template>
+                    </tr>
+                  </tbody>
+                </template>
+              </v-simple-table>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
 
-    <!-- Navigation -->
-    <v-row justify="space-between" class="mt-4">
-      <v-col cols="auto">
-        <v-btn
-          text
-          :to="steps[2].route"
-        >
-          <v-icon left>mdi-arrow-left</v-icon>
-          Back
-        </v-btn>
-      </v-col>
-      <v-col cols="auto">
-        <v-btn
-          color="primary"
-          @click="proceed"
-          :to="steps[4].route"
-        >
-          Next 
-          <v-icon right>mdi-arrow-right</v-icon>
-        </v-btn>
-      </v-col>
-    </v-row>
+      <!-- Navigation -->
+      <v-row justify="space-between" class="mt-4">
+        <v-col cols="auto">
+          <v-btn
+            text
+            :to="steps[2].route"
+          >
+            <v-icon left>mdi-arrow-left</v-icon>
+            Back
+          </v-btn>
+        </v-col>
+        <v-col cols="auto">
+          <v-btn
+            color="primary"
+            @click="proceed"
+            :to="steps[4].route"
+          >
+            Next 
+            <v-icon right>mdi-arrow-right</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </template>
   </v-container>
 </template>
 
@@ -95,7 +143,8 @@ export default {
   data() {
     return {
       fieldSettings,
-      previewData: []
+      previewData: [],
+      isInitialized: false
     }
   },
 
@@ -104,21 +153,49 @@ export default {
     ...mapGetters('bulk-upload', ['getAllFieldMappings', 'allFields'])
   },
 
-  created() {
-    this.processPreviewData()
+  async created() {
+    // Wait for store initialization on client side
+    if (process.client && this.$store.$initBulkUpload) {
+      await this.$store.$initBulkUpload()
+    }
+    
+    await this.processPreviewData()
+    this.isInitialized = true
   },
 
   methods: {
     ...mapMutations('bulk-upload', ['completeStep', 'setCurrentStep']),
 
-    processPreviewData() {
+    getSubFieldMapping(field, subField) {
+      const mapping = this.getAllFieldMappings[field]
+      if (!mapping) return null
+
+      const valueKeys = {
+        'csv-column': 'csvValue',
+        'phaidra-field': 'phaidraValue'
+      }
+      
+      // For single fields
+      if (!subField) {
+        return mapping[valueKeys[mapping.source]] || null
+      }
+      
+      // For multi-fields
+      if (fieldSettings[field]?.fieldType === 'multi-field') {
+        if (!mapping.subFields) return null
+        return mapping.subFields[subField]?.[valueKeys[mapping.source]] || null
+      }
+      
+      return null
+    },
+
+    async processPreviewData() {
       if (!this.csvContent) return
 
       const rows = this.csvContent.split('\n')
       const headers = rows[0].split(';').map(h => h.trim().replace(/["']/g, ''))
-      
-      // Process only first 5 rows for preview
-      const previewRows = rows.slice(1, 6).map(row => {
+
+      const previewRows = rows.map(row => {
         const values = row.split(';').map(v => v.trim().replace(/["']/g, ''))
         const rowData = {}
         
@@ -129,17 +206,47 @@ export default {
             rowData[field] = ''
           } else if (mapping.source === 'phaidra-field') {
             if (field === "License") {
-              rowData[field] = mapping.phaidraValue["@id"]
+              rowData[field] = mapping.phaidraValue?.["@id"] || ''
             }
             else if (field === "Type") {
-              rowData[field] = mapping.phaidraValue["skos:prefLabel"]["eng"]
+              rowData[field] = mapping.phaidraValue?.["skos:prefLabel"]?.["eng"] || ''
+            }
+            else if (fieldSettings[field]?.fieldType === 'multi-field') {
+              rowData[field] = {}
+              Object.keys(fieldSettings[field].multiFieldConfig.fields).forEach(subField => {
+                if (!mapping.subFields) {
+                  rowData[field][subField] = ''
+                } else if (subField === "Role") {
+                  rowData[field][subField] = mapping.subFields[subField]?.phaidraValue?.["skos:prefLabel"]?.["eng"] || ''
+                }
+                else {
+                  rowData[field][subField] = mapping.subFields[subField]?.phaidraValue || ''
+                }
+              })
             }
             else {
-              rowData[field] = mapping.phaidraValue
+              rowData[field] = mapping.phaidraValue || ''
             }
           } else if (mapping.source === 'csv-column') {
-            const columnIndex = headers.indexOf(mapping.csvValue)
-            rowData[field] = columnIndex >= 0 ? values[columnIndex] : ''
+            if (fieldSettings[field]?.fieldType === 'multi-field') {
+              rowData[field] = {}
+              Object.keys(fieldSettings[field].multiFieldConfig.fields).forEach(subField => {
+                if (!mapping.subFields) {
+                  rowData[field][subField] = ''
+                } else {
+                  const subMapping = mapping.subFields[subField]
+                  if (subMapping?.csvValue) {
+                    const columnIndex = headers.indexOf(subMapping.csvValue)
+                    rowData[field][subField] = columnIndex >= 0 ? values[columnIndex] : ''
+                  } else {
+                    rowData[field][subField] = ''
+                  }
+                }
+              })
+            } else {
+              const columnIndex = headers.indexOf(mapping.csvValue)
+              rowData[field] = columnIndex >= 0 ? values[columnIndex] : ''
+            }
           }
         })
         
@@ -149,14 +256,24 @@ export default {
       this.previewData = previewRows
     },
 
-    getSourceInfo(field) {
+    getSourceInfo(field, subField = null) {
       const mapping = this.getAllFieldMappings[field]
       if (!mapping) return null
       
       if (mapping.source === 'phaidra-field') {
         return 'Default value sourced from Phaidra'
       }
-      return 'Sourced from CSV column: ' + mapping.csvValue
+      else if (mapping.source === 'csv-column') {
+        if (fieldSettings[field]?.fieldType === 'multi-field' && subField) {
+          if (!mapping.subFields) return null
+          const subMapping = mapping.subFields[subField]
+          return subMapping?.csvValue ? `Sourced from CSV column: ${subMapping.csvValue}` : null
+        }
+        else {
+          return mapping.csvValue ? `Sourced from CSV column: ${mapping.csvValue}` : null
+        }
+      }
+      return null
     },
 
     proceed() {
